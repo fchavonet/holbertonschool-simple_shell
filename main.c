5c37d541dd5b5b85f3f341b6ed53d9b38d25738d
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-08-17 09:32:05 +0000

    Changes backup

--- main.c
+++ main.c
@@ -17,41 +17,70 @@ int free_buffers(char *user_input, char *cmd)
 	return (0);
 }
 
+// /**
+//  * execute_cmd - executes a command from the given path string
+//  * @path: given path string
+//  *
+//  * Return: execve return value
+// */
+// int execute_cmd()
+
 /**
  * main - temporary, for testing purposes
  *
  * Return: 0
  */
-int main(void)
+int main(int ac, char **av, char **env)
 {
-	size_t max_cmd_length = 1024;
+	size_t max_cmd_length = 4096;
 	//atoi(getenv("ARG_MAX"));
 	char *user_input = NULL;
 	char *cmd = NULL;
 	char *token = NULL;
+	ssize_t getline_result = 0;
+	int execve_result = 0;
+	pid_t child_pid = 0;
+	int status = 0;
 
-	// user_input = malloc(sizeof(char) * max_cmd_length);
-	// cmd = malloc(sizeof(char) * max_cmd_length);
+	char *tmp_av[2];
+	tmp_av[0] = "-1";
+	tmp_av[1] = NULL;
 
 	while (1)
 	{
 		printf("$ ");
-		if (getline(&cmd, &max_cmd_length, stdin) == 0)
+		getline_result = getline(&cmd, &max_cmd_length, stdin);
+		if (getline_result == -1)
 		{
-			perror("getline");
+			perror("getline process failed.");
 			exit(EXIT_FAILURE);
 		}
+		else if (getline_result == EOF)
+		{
+			printf("\n");
+			exit(EXIT_SUCCESS);
+		}
 
-		if (strncmp(cmd, "exit", 4) == 0)
+		token = strtok(cmd, " ");
+		if (strcmp(token, "exit") == 0)
 		{
 			free_buffers(user_input, cmd);
 			exit(EXIT_SUCCESS);
 		}
-		token = strtok(cmd, " ");
-		// while (token != NULL)
-		// {
-		// }
-		printf("Token : %s\n", token);
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			execve_result = execve(token, tmp_av, env);
+			exit(1);
+		}
+		else if (child_pid > 0)
+		{
+			wait(&status);
+		}
+		else
+		{
+			perror("execve process failed.");
+		}
 	}
 	free_buffers(user_input, cmd);
 	return (0);
