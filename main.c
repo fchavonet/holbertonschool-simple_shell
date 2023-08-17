640400052148e925b83f68a45972c910e5f8279d
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-08-17 13:54:35 +0000

    WIP backup

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
  * free_buffers - frees allocated variables
  *
  * Return: 0
-*/
-int free_buffers(char *user_input, char *cmd)
+ */
+int free_buffers(char *user_input, char *cmd, char **args)
 {
 	if (user_input != NULL)
 	{
@@ -14,7 +14,47 @@ int free_buffers(char *user_input, char *cmd)
 	{
 		free(cmd);
 	}
+	if (args != NULL)
+	{
+		free(args);
+	}
 	return (0);
+}
+
+/**
+ * build_args - initialize an array of strings containing arguments
+ * @cmd: given command string
+ *
+ * Return: pointer to created array of args
+ */
+char **build_args(char *cmd)
+{
+	unsigned int i = 0;
+	unsigned int count = 1;
+	char *token;
+	char ** args = NULL;
+
+	while (cmd[i] != '\0')
+	{
+		if (cmd[i] = ' ')
+		{
+			count++;
+		}
+		i++;
+	}
+
+	args = malloc(sizeof(char *) * count);
+	if (args == NULL)
+	{
+		perror("Memory allocation failed");
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	while (token != NULL)
+	{
+		token = strtok(NULL, " ");
+		args[i] = strdup(token);
+	}
 }
 
 // /**
@@ -33,7 +73,7 @@ int free_buffers(char *user_input, char *cmd)
 int main(int ac, char **av, char **env)
 {
 	size_t max_cmd_length = 4096;
-	//atoi(getenv("ARG_MAX"));
+	// atoi(getenv("ARG_MAX"));
 	char *user_input = NULL;
 	char *cmd = NULL;
 	char *token = NULL;
@@ -41,36 +81,34 @@ int main(int ac, char **av, char **env)
 	int execve_result = 0;
 	pid_t child_pid = 0;
 	int status = 0;
+	char ** args = NULL;
 
-	char *tmp_av[2];
-	tmp_av[0] = "-1";
-	tmp_av[1] = NULL;
+	// char *tmp_av[2];
+	// tmp_av[0] = "-1";
+	// tmp_av[1] = NULL;
 
 	while (1)
 	{
 		printf("$ ");
 		getline_result = getline(&cmd, &max_cmd_length, stdin);
-		if (getline_result == -1)
-		{
-			perror("getline process failed.");
-			exit(EXIT_FAILURE);
-		}
-		else if (getline_result == EOF)
+		if (getline_result == EOF)
 		{
 			printf("\n");
 			exit(EXIT_SUCCESS);
 		}
-
 		token = strtok(cmd, " ");
-		if (strcmp(token, "exit") == 0)
+		//token[strlen(token) - 1] = '\0';
+		printf("[%s] is %lu chars long.\n", cmd, strlen(cmd));
+		if (strcmp(token, "exit\n") == 0)
 		{
-			free_buffers(user_input, cmd);
+			free_buffers(user_input, cmd, args);
 			exit(EXIT_SUCCESS);
 		}
+		args = build_args(cmd);
 		child_pid = fork();
 		if (child_pid == 0)
 		{
-			execve_result = execve(token, tmp_av, env);
+			execve_result = execve(token, args, env);
 			exit(1);
 		}
 		else if (child_pid > 0)
@@ -82,6 +120,6 @@ int main(int ac, char **av, char **env)
 			perror("execve process failed.");
 		}
 	}
-	free_buffers(user_input, cmd);
+	free_buffers(user_input, cmd, args);
 	return (0);
 }
